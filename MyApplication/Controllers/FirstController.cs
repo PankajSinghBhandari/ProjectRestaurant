4a0bcb7ee56095e30e6c71514a2128f21fa99deb
pankaj1 <pankaj1@PANKAJ>
2016-12-13 20:37:03 -0800

    second commit controller added(first controller)

---
+++ MyApplication/Controllers/FirstController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApplication.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        // Starting my first controller work
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
