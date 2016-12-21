e035114875ba51c3021dfc1fe61db8d8ca6b5703
pankaj1 <pankaj1@PANKAJ>
2016-12-21 22:37:26 +0530

    Structure change

---
+++ MyApplication/Controllers/CustomerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyApplication.Models;
+
+namespace MyApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ResturantEntities db = new ResturantEntities();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(db.CustomerDetails.ToList());
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerDetail customerDetail = db.CustomerDetails.Find(id);
+            if (customerDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerDetail);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerId,Name,ContactNo,OrderId,Price,Comment,CreatedDate,ModifiedDate")] CustomerDetail customerDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CustomerDetails.Add(customerDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customerDetail);
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerDetail customerDetail = db.CustomerDetails.Find(id);
+            if (customerDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerDetail);
+        }
+
+        // POST: Customer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerId,Name,ContactNo,OrderId,Price,Comment,CreatedDate,ModifiedDate")] CustomerDetail customerDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customerDetail);
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerDetail customerDetail = db.CustomerDetails.Find(id);
+            if (customerDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerDetail);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CustomerDetail customerDetail = db.CustomerDetails.Find(id);
+            db.CustomerDetails.Remove(customerDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyApplication/Models/OurCustomerDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApplication.Models
+{
+    public class OurCustomerDetail
+    {
+
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string ContactNo { get; set; }
+        public string OrderId { get; set; }
+        public string Price { get; set; }
+        public string Comment { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
